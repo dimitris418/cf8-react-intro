ca1ee310180da309ea4bb4dd62fa3e392ec2e73c
dimitris418 <dimitris_matthioudakis@hotmail.com>
2025-09-26 15:22:01 +0300

    feat: add class and functional components

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,5 @@
+const ArrowFunctionalComponent = () => {
+    const title = "I am an arrow functional component";
+    return <h1 className= "text-center text-xl font-bold mt-12">{title}</h1>
+}
+export default ArrowFunctionalComponent;
---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,5 @@
+function FunctionalComponent() {
+    const title = "I am a functional component";
+    return <h1 className= "text-center text-xl font-bold mt-12">{title}</h1>
+}
+export default FunctionalComponent;
